27f62d7aec87fc89b26750e65b34a7e645b4d47e
Tudou4161 <cef5787@gamil.com>
2020-10-30 17:02:28 +0900

    1702_commit

--- stts_KRtutor/QnA/urls.py
+++ stts_KRtutor/QnA/urls.py
@@ -8,8 +8,10 @@ app_name = "QnA"
 
 urlpatterns = [
     path('', QnA_list, name="QnA_list"),
+    path('', MyQnA, name="MyQnA"),
     path('detail/<int:pk>/', DetailView.as_view(model=QuestionAndAnswer, template_name="QnA/detail.html"), name="QnA_detail"),
     path('upload/', QnAUploadView.as_view(), name="QnA_upload"),
     path('update/<int:pk>/', QnAUpdateView.as_view(), name="QnA_update"),
     path('delete/<int:pk>/', QnADeleteView.as_view(), name="QnA_delete"),
 ]
+
--- stts_KRtutor/QnA/views.py
+++ stts_KRtutor/QnA/views.py
@@ -3,11 +3,20 @@ from .models import QuestionAndAnswer
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 # Create your views here.
 
+
 def QnA_list(request):
     QnAs = QuestionAndAnswer.objects.all().order_by("hit")
-    
 
     return render(request, "QnA/list.html", context={"QnAs": QnAs})
+
+
+def MyQnA(request):
+    userid = request.user
+    
+    MyQnAs = QuestionAndAnswer.objects.filter(author=userid.id).order_by("hit")
+        
+    return render(request, "QnA/myqna.html", context={"MyQnAs": MyQnAs})
+
 
 class QnAUploadView(CreateView):
     model = QuestionAndAnswer
