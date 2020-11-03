581ecb3ee321abdd208dd591ca0df3dedaa523ff
Tudou4161 <cef5787@gamil.com>
2020-11-03 18:03:44 +0900

    1803_commit

--- stts_KRtutor/QnA/urls.py
+++ stts_KRtutor/QnA/urls.py
@@ -8,8 +8,9 @@ app_name = "QnA"
 
 urlpatterns = [
     path('', QnA_list, name="QnA_list"),
-    path('', MyQnA, name="MyQnA"),
+    path('myqna/', MyQnA, name="QnA_myqna"),
     path('detail/<int:pk>/', DetailView.as_view(model=QuestionAndAnswer, template_name="QnA/detail.html"), name="QnA_detail"),
+    path('Mydetail/<int:pk>/', DetailView.as_view(model=QuestionAndAnswer, template_name="QnA/Mydetail.html"), name="MyQnA_detail"),
     path('upload/', QnAUploadView.as_view(), name="QnA_upload"),
     path('update/<int:pk>/', QnAUpdateView.as_view(), name="QnA_update"),
     path('delete/<int:pk>/', QnADeleteView.as_view(), name="QnA_delete"),
